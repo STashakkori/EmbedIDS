3c1c4ec0c6f9fbfc251d33d00684f5468edaef2e
$t@$h <sinatashakkori@gmail.com>
2025-06-28 22:04:56 -0400

    Create types.rs

---
+++ types.rs
@@ -0,0 +1,6 @@
+// $t@$h
+pub const MQTT_CLIENT_ID: &str = "rust_mqtt";
+pub const MQTT_BROKER_ADDRESS: &str = "broker.hivemq.com";
+pub const MQTT_BROKER_PORT: u16 = 1883;
+pub const IDS_UPLOAD_TOPIC: &str = "ids/upload";
+pub const IDS_INGRESS_TOPIC: &str = "ids/ingress";
